68e57c5faec0fdc734b74f3e29750c9f23c9d66e
adenuga emmanuel <ehmmygal@gmail.com>
2025-03-08 18:47:40 -1200

    seventh commit

--- client/client/src/pages/FoodList.jsx
+++ client/client/src/pages/FoodList.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import starImg from '../assets/starIcon.svg'
+
 const burgerData = [
     {
         _id: 1,
@@ -78,21 +78,21 @@ const burgerData = [
 const BurgerGrid = () => {
     return (
         <div className="bg-[#2F2F2F] p-4">
-            <div className="container mx-auto max-w-[1100px] rounded-[8px] p-4 ">
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
+            <div className="container mx-auto max-w-[1100px] rounded-[8px] p-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                     {burgerData.map((burger) => (
                         <div key={burger._id} className="rounded-lg p-4 shadow-lg bg-[#252422]">
-                            <img src={burger.image} alt={burger.title} className="w-[349.33px] h-[321px] object-cover rounded-[8px] mb-4" />
+                            <img src={burger.image} alt={burger.title} className="w-full h-[321px] object-cover rounded-[8px] mb-4" />
                             <div className="flex justify-between">
                                 <h2 className="text-lg text-[20px] text-white">{burger.title}</h2>
                                 <p className="text-gray-400 border border-[#B67B0F] gap-4 rounded-[6px] py-[6px] px-[4px]">⭐{burger.rating}</p>
                             </div>
 
-                            <div className="flex justify-between ">
+                            <div className="flex justify-between">
                                 <p className="text-[#B67B0F] font-bold text-[25px] pt-[15px]"><span className="font-bold text-[15px]">₦</span>{burger.price}</p>
                                 <p className="text-gray-400 pt-[20px]">{burger.duration}</p>
                             </div>
-                            <button className="py-[15px] px-[56px] rounded-[31px] btn-active bg-[#B67B0F] w-full gap-7 text-white">Add To Cart</button>
+                            <button className="py-[15px] px-[56px] rounded-[31px] bg-[#B67B0F] w-full text-white">Add To Cart</button>
                         </div>
                     ))}
                 </div>
@@ -100,4 +100,5 @@ const BurgerGrid = () => {
         </div>
     );
 };
+
 export default BurgerGrid;
